bd843c206219ec7332cc6ebd6d2edca0bbf76647
Yue240383059 <2531180w@student.gla.ac.uk>
2022-02-03 17:34:53 +0000

    commit

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from rango import views
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ]
