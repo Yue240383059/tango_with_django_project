13b291005279402ebcea9714d6905ca9064fb34a
Yue240383059 <2531180w@student.gla.ac.uk>
2022-02-21 01:54:24 +0000

    commit

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -21,5 +21,6 @@ from django.urls import include
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
+    # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ]
