63e0cb8f516c2c92e59d55e9c7fd5462b0523e72
Yue240383059 <2531180w@student.gla.ac.uk>
2022-02-22 07:00:11 +0000

    chapter8

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -87,3 +87,4 @@ def add_page(request, category_name_slug):
             print(form.errors)
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
+    
